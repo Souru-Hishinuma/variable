4ed9103b18461f4fdd276a2829bab9702501223b
Souru-Hishinuma <sourunrun@gmail.com>
2022-06-26 01:29:50 +0900

    let, const, var

---
+++ index.js
@@ -0,0 +1,26 @@
+// const,letについて
+
+// varによる宣言
+var value = "var";
+console.log(value);
+
+value = "var上書き";
+console.log(value);
+
+var value = "再宣言";
+console.log(value);
+
+// letによる宣言
+let value2 = "let";
+console.log(value2);
+
+value2 = "let上書き";
+console.log(value2);
+
+let value2 = "再宣言";
+console.log(value2);
+
+const value3 = "const変数";
+console.log(value3);
+
+value3 = "const上書き";
